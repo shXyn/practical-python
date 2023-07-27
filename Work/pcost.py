94a821f68c36eae009edca093a91de4a1b4af78c
Henry Tse <henrytse2003@gmail.com>
2023-07-27 13:42:57 -0400

    Completed E1.33

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,19 +2,29 @@
 #
 # Exercise 1.27
 
+import sys
+import csv
+
 def portfolio_cost(filename):
 
     total = 0
 
-    f = open(filename , 'rt')
-    headers = next(f)
-
-    for line in f:
-        dataList = line.split(',')
-        total += (int(dataList[1]) * float(dataList[2]))
+    f = open(filename)
+    #headers = next(f)
+    rows = csv.reader(f)
+    for row in rows:
+        try:
+            total += (int(row[1]) * float(row[2]))
+        except:
+            print('Invalid Line: ', row)
     f.close()
     return total
 
-cost = portfolio_cost('Data/portfolio.csv')
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
 print('Total cost', cost)
 
