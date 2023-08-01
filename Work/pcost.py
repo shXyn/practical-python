962b21adabfe6a1404c3c1edeeb686f1a82f48aa
Henry Tse <henrytse2003@gmail.com>
2023-08-01 11:39:11 -0400

    Completed E2.15

--- Work/pcost.py
+++ Work/pcost.py
@@ -10,20 +10,20 @@ def portfolio_cost(filename):
     total = 0
 
     f = open(filename)
-    #headers = next(f)
+    headers = next(f)
     rows = csv.reader(f)
-    for row in rows:
+    for row,line in enumerate(rows):
         try:
-            total += (int(row[1]) * float(row[2]))
-        except:
-            print('Invalid Line: ', row)
+            total += (int(line[1]) * float(line[2]))
+        except ValueError:
+            print(f'Row {row}: Bad row: {line}')
     f.close()
     return total
 
 if len(sys.argv) == 2:
     filename = sys.argv[1]
 else:
-    filename = 'Data/portfolio.csv'
+    filename = 'Data/missing.csv'
 
 cost = portfolio_cost(filename)
 print('Total cost', cost)
